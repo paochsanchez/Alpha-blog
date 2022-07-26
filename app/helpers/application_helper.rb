61696ec361842c9ffa8f9b622e2c61f12e01be27
paochsanchez <pchinchilla@estudiantec.com>
2022-07-25 18:25:28 -0600

    Auth methods as helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  
+
     def gravatar_for(user, options = { size: 80})
       email_address = user.email.downcase
       hash = Digest::MD5.hexdigest(email_address)
@@ -8,11 +8,5 @@ module ApplicationHelper
       image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
     end
 
-    def current_user
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    end
-  
-    def logged_in?
-      !!current_user
-    end
+
 end
