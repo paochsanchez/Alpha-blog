49fa141358d9b37c96848c3f0dd013719bd1809a
paochsanchez <pchinchilla@estudiantec.com>
2022-07-26 00:02:32 -0600

    Restrict controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update]
+
   def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
@@ -8,20 +11,8 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page], per_page: 5)
   end
 
-  
   def new
     @user = User.new
-  end
-  
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
-      flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
-      redirect_to articles_path
-    else
-      render 'new'
-    end
   end
 
   def edit
@@ -35,7 +26,18 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-  
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
@@ -44,4 +46,12 @@ class UsersController < ApplicationController
   def set_user
     @user = User.find(params[:id])
   end
+
+  def require_same_user
+    if current_user != @user
+      flash[:alert] = "You can only edit your own account"
+      redirect_to @user
+    end
   end
+  
+end
